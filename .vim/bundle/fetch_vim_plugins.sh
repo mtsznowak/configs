288797b0b01210c675f493abe60175a5e0a5d27a
Mateusz Nowak <mtsznowak9@gmail.com>
2017-10-26 22:32:31 +0200

    add script fetching vim plugins

---
+++ .vim/bundle/fetch_vim_plugins.sh
@@ -0,0 +1,12 @@
+# install pathogen
+mkdir -p ~/.vim/autoload
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# fetch plugins
+git clone https://github.com/elixir-editors/vim-elixir.git
+git clone https://github.com/tpope/vim-sleuth.git
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/jistr/vim-nerdtree-tabs.git
+git clone https://github.com/vim-scripts/Ambient-Color-Scheme.git
+git clone https://github.com/slashmili/alchemist.vim.git
+git clone https://github.com/scrooloose/nerdtree.git
